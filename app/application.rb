5f4a55fb2b4efa27b4a586eebb36a386b69b65b7
zachary mines <zmines@wesleyan.edu>
2020-08-31 02:58:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
+    num_1 = Kernel.rand(1..2)
+    num_2 = Kernel.rand(1..2)
+    num_3 = Kernel.rand(1..2)
     
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
